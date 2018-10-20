07a939e4bcbcbc40195ceab33a72bf27a7621a4b
Alejandro Agudelo <carlosaguirre7@hotmail.com>
2018-10-20 13:02:44 -0500

    Fix: Entidad Producto terminada

--- rappi poderoso/rappi poderoso/Producto.swift
+++ rappi poderoso/rappi poderoso/Producto.swift
@@ -20,8 +20,14 @@ struct Producto {
     var product_id: Int
     
     init?(json: [String: Any]) {
-        guard let image = json["image"] as? String,
-            let name = json["name"] as? String
+        guard let _id = json["_id"] as? String,
+            let name = json["name"] as? String,
+            let image = json["image"] as? String,
+            let description = json["description"] as? String,
+            let store_id = json["store_id"] as? Int,
+            let price = json["price"] as? Float,
+            let product_id = json["product_id"] as? Int
+        
             else {
                 return nil
         }
@@ -29,11 +35,11 @@ struct Producto {
         self.image = image
         self.name = name
         self.corridors = nil
-        self._id = ""
-        self.description = ""
-        self.store_id = 0
-        self.price = 0
-        self.product_id = 0
+        self._id = _id
+        self.description = description
+        self.store_id = store_id
+        self.price = price
+        self.product_id = product_id
                 
     }
 
