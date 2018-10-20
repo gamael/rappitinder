8d363f93b7fa411efa796a61e9bac72a9ba22065
Alejandro Agudelo <carlosaguirre7@hotmail.com>
2018-10-20 11:21:33 -0500

    Feature: Entidades Corredor y Producto implementadas

---
+++ rappi poderoso/rappi poderoso/Corredor.swift
@@ -0,0 +1,16 @@
+//
+//  Corredor.swift
+//  rappi poderoso
+//
+//  Created by Alejandro Agudelo on 20/10/18.
+//  Copyright © 2018 Alejandro Agudelo. All rights reserved.
+//
+
+import Foundation
+
+struct Corredor {
+    
+    var parent_id: Int?
+    var id: Int
+    var name: String
+}
